9e467923e5de01ce2daa22c4eaacb2cc2a4784ac
harshithaRao5 <harshithanallani@msitprogram.net>
2019-05-06 22:58:27 +0530

    m1

--- module1/ReplaceTheSpaces.c
+++ module1/ReplaceTheSpaces.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 void main()
 {
 	/* code */
-
+	char string[1000], stringop[1000];
+	printf("enter the string\n");
+	gets(string);
+	int j = 0;
+	for (int i = 0; i < strlen(string); i++)
+	{
+		if(string[i]==' '&&string[i+1]!=' '){
+			stringop[j++]=string[i];
+		}else if(string[i]!=' '){
+			stringop[j++]=string[i];
+		}
+	}
+	printf("required output string is..\n");
+	printf(stringop);
 }
